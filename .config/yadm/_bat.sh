3aec6d976fbe0f3bbc10b1448c8f5111a7d885a2
Jesse Cooke <jesse@relativepath.io>
2021-02-19 12:51:27 -0800

    bootstrap: install clean up bat & alacritty install scripts

--- .config/yadm/_bat.sh
+++ .config/yadm/_bat.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 DIR="${BASH_SOURCE%/*}" || if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
+# shellcheck disable=SC1090,SC1091
 source "$DIR/_helpers.sh"
 
 log "Installing bat"
@@ -8,15 +9,15 @@ installed_version="$(bat --version | cut -d " " -f 2)"
 latest_version="$(latest_release_tag_name "sharkdp" "bat")"
 bat_version="0.17.1"
 
-if [ $latest_version != "v$installed_version" ]; then
+if [ "$latest_version" != "v$installed_version" ]; then
   echo "Installed: $installed_version"
   echo "Latest: $latest_version"
 
-  cd $HOME/src
+  cd "$HOME/src" || exit 1
   bat="bat_${bat_version}_amd64"
   bat_deb="$bat.deb"
   wget "https://github.com/sharkdp/bat/releases/download/v$bat_version/$bat_deb"
   sudo dpkg -i "$bat_deb"
 else
-  echo "Already on latest"
+  echo "Already on latest: $bat_version"
 fi
