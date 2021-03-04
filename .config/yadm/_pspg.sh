35a805e72a390eed93386c48b66c23bc358e6e4d
Jesse Cooke <jesse@relativepath.io>
2021-03-04 07:10:15 -0800

    Switch pspg from lagging repo to source

---
+++ .config/yadm/_pspg.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+DIR="${BASH_SOURCE%/*}" || if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
+# shellcheck disable=SC1090,SC1091
+source "$DIR/_helpers.sh"
+
+log "Installing pspg"
+set -x
+installed_version="$(pspg --version | head -n 1 | cut -d "-" -f 2)"
+latest_version="$(latest_release_tag_name "okbob" "pspg")"
+pspg_version="4.3.0"
+
+if [ "$latest_version" != "$installed_version" ]; then
+  echo "Installed: $installed_version"
+  echo "Latest: $latest_version"
+
+  cd "$HOME/src" || exit 1
+  wget "https://github.com/okbob/pspg/archive/$pspg_version.tar.gz"
+  tar xf "$pspg_version.tar.gz"
+  ./configure
+  make && sudo make install
+else
+  echo "Already on latest: $pspg_version"
+fi
