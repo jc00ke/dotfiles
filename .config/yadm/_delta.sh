5d0581a8bfaa65c5347cab5d2cf273cdb2aa94d7
Jesse Cooke <jesse@relativepath.io>
2021-03-18 07:31:13 -0700

    bootstrap: clean up delta install script

--- .config/yadm/_delta.sh
+++ .config/yadm/_delta.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
 DIR="${BASH_SOURCE%/*}" || if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
+# shellcheck disable=SC1090,SC1091
 source "$DIR/_helpers.sh"
 
 log "Installing delta"
-cd $HOME/src
-delta_version="0.4.4"
-delta="git-delta-musl_${delta_version}_amd64"
-delta_deb="$delta.deb"
-wget "https://github.com/dandavison/delta/releases/download/$delta_version/$delta_deb"
-sudo dpkg -i "$delta_deb"
+installed_version="$(delta --version | cut -d " " -f 2)"
+latest_version="$(latest_release_tag_name "dandavison" "delta")"
+delta_version="0.6.0"
+
+if [ "$latest_version" != "v$installed_version" ]; then
+  echo "Installed: $installed_version"
+  echo "Latest: $latest_version"
+
+  cd "$HOME/src" || exit 1
+  delta="git-delta-musl_${delta_version}_amd64"
+  delta_deb="$delta.deb"
+  wget "https://github.com/dandavison/delta/releases/download/$delta_version/$delta_deb"
+  sudo dpkg -i "$delta_deb"
+else
+  echo "Already on latest: $delta_version"
+fi
