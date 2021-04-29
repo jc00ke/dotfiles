272e0bc6aad736b384db404f7d70140ad5af3f10
Jesse Cooke <jesse@relativepath.io>
2021-04-29 12:54:38 -0700

    bootstrap: update zoxide to 0.6.0
    Also improve the install script

--- .config/yadm/_zoxide.sh
+++ .config/yadm/_zoxide.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
 DIR="${BASH_SOURCE%/*}" || if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
+# shellcheck disable=SC1090,SC1091
 source "$DIR/_helpers.sh"
 
 log "Installing zoxide"
-cd $HOME/src
-zoxide_version="0.5.0"
-zoxide="zoxide-x86_64-unknown-linux-musl"
-wget "https://github.com/ajeetdsouza/zoxide/releases/download/v$zoxide_version/$zoxide"
-sudo mv "$zoxide" "/usr/local/bin/zoxide"
-sudo chmod +x "/usr/local/bin/zoxide"
+installed_version="$(zoxide -V | cut -d " " -f 2)"
+latest_version="$(latest_release_tag_name "ajeetdsouza" "zoxide")"
+zoxide_version="0.6.0"
+
+if [ "$latest_version" != "$installed_version" ]; then
+  echo "Installed: $installed_version"
+  echo "Latest: $latest_version"
+
+  cd "$HOME/src" || exit 1
+  zoxide="zoxide-x86_64-unknown-linux-musl"
+  wget "https://github.com/ajeetdsouza/zoxide/releases/download/v$zoxide_version/$zoxide"
+  sudo mv "$zoxide" "/usr/local/bin/zoxide"
+  sudo chmod +x "/usr/local/bin/zoxide"
+else
+  echo "Already on latest: $zoxide_version"
+fi
