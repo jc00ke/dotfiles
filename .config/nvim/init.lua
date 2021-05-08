3d2791cd8f5e0d0126c8d7c1e4e3fff7ad18a977
Jesse Cooke <jesse@relativepath.io>
2021-05-08 11:29:10 -0700

    nvim: get completion working with compe

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -71,18 +71,6 @@ function create_augroup(autocmds, name)
     cmd('augroup END')
 end
 
-local function t(str)
-    return vim.api.nvim_replace_termcodes(str, true, true, true)
-end
-
-function _G.smart_tab()
-    return vim.fn.pumvisible() == 1 and t'<C-n>' or t'<Tab>'
-end
-
-function _G.smart_shift_tab()
-    return vim.fn.pumvisible() == 1 and t'<C-p>' or t'<S-Tab>'
-end
-
 -- Indentation
 g.mapleader = ","
 g.localleader = "\\"
@@ -108,7 +96,7 @@ opt('b', 'smartindent', true)                         -- Insert indents automati
 opt('b', 'tabstop', indent)                           -- Number of spaces tabs count for
 opt('b', 'swapfile', false)                           -- Swapfile
 opt('b', 'textwidth', 110)                            -- Match GitHub's 110 char width
-opt('o', 'completeopt', 'menuone,noinsert,noselect')  -- Completion options (for deoplete)
+opt('o', 'completeopt', 'menuone,noselect')           -- Completion options (for deoplete)
 opt('o', 'hidden', true)                              -- Enable modified buffers in background
 opt('o', 'ignorecase', true)                          -- Ignore case
 opt('o', 'joinspaces', false)                         -- No double spaces with join after a dot
@@ -171,6 +159,7 @@ end
 vim.cmd 'packadd paq-nvim'            -- Load package
 local paq = require('paq-nvim').paq   -- Import module and bind `paq` function
 paq{'savq/paq-nvim', opt=true}        -- Let Paq manage itself
+paq 'nvim-treesitter/nvim-treesitter'
 paq 'tpope/vim-sensible'
 paq 'tpope/vim-obsession'
 paq 'kyazdani42/nvim-tree.lua'
@@ -222,6 +211,7 @@ g["rustfmt_autosave"] = 1
 
 -- utils
 paq 'nvim-lua/plenary.nvim'
+RELOAD = require('plenary.reload').reload_module
 
 -- sessions
 paq 'tpope/vim-obsession'
@@ -315,47 +305,95 @@ paq 'gennaro-tedesco/nvim-jqx'
 
 -- LSP
 paq 'neovim/nvim-lspconfig'
-paq 'kabouzeid/nvim-lspinstall'
-require'lspinstall'.setup() -- important
+paq 'hrsh7th/nvim-compe'
 
-local servers = require('lspinstall').installed_servers()
-for _, server in pairs(servers) do
-  require'lspconfig'[server].setup{}
+map("n", "gd",    [[<cmd>lua vim.lsp.buf.definition()<CR>]], { silent = true })
+map("n", "gD",    [[<cmd>lua vim.lsp.buf.declaration()<CR>]], { silent = true })
+map("n", "gr",    [[<cmd>lua vim.lsp.buf.references()<CR>]], { silent = true })
+map("n", "gi",    [[<cmd>lua vim.lsp.buf.implementation()<CR>]], { silent = true })
+map("n", "K",     [[<cmd>lua vim.lsp.buf.hover()<CR>]], { silent = true })
+map("n", "<C-k>", [[<cmd>lua vim.lsp.buf.signature_help()<CR>]], { silent = true })
+map("n", "<C-n>", [[<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>]], { silent = true })
+map("n", "<C-p>", [[<cmd>lua vim.lsp.diagnostic.goto_next()<CR>]], { silent = true })
+
+
+cmd([[autocmd BufWritePre *.js lua vim.lsp.buf.formatting_sync(nil, 100)]])
+cmd([[autocmd BufWritePre *.jsx lua vim.lsp.buf.formatting_sync(nil, 100)]])
+cmd([[autocmd BufWritePre *.py lua vim.lsp.buf.formatting_sync(nil, 100)]])
+cmd([[autocmd BufWritePre *.ex lua vim.lsp.buf.formatting_sync(nil, 100)]])
+cmd([[autocmd BufWritePre *.exs lua vim.lsp.buf.formatting_sync(nil, 100)]])
+
+require('compe').setup {
+  enabled = true;
+  autocomplete = true;
+  debug = false;
+  min_length = 1;
+  preselect = 'enable';
+  throttle_time = 80;
+  source_timeout = 200;
+  incomplete_delay = 400;
+  max_abbr_width = 100;
+  max_kind_width = 100;
+  max_menu_width = 100;
+  documentation = false;
+
+  source = {
+    path = true;
+    buffer = true;
+    calc = true;
+    --vsnip = true;
+    nvim_lsp = true;
+    nvim_lua = true;
+    spell = true;
+    tags = true;
+    snippets_nvim = true;
+    treesitter = true;
+  };
+}
+
+local t = function(str)
+  return vim.api.nvim_replace_termcodes(str, true, true, true)
 end
 
-local function setup_servers()
-  require'lspinstall'.setup()
-  local servers = require'lspinstall'.installed_servers()
-  for _, server in pairs(servers) do
-    require('lspconfig')[server].setup{}
+local check_back_space = function()
+    local col = vim.fn.col('.') - 1
+    if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
+        return true
+    else
+        return false
+    end
+end
+
+-- Use (s-)tab to:
+--- move to prev/next item in completion menuone
+--- jump to prev/next snippet's placeholder
+_G.tab_complete = function()
+  if vim.fn.pumvisible() == 1 then
+    return t "<C-n>"
+  elseif vim.fn.call("vsnip#available", {1}) == 1 then
+    return t "<Plug>(vsnip-expand-or-jump)"
+  elseif check_back_space() then
+    return t "<Tab>"
+  else
+    return vim.fn['compe#complete']()
+  end
+end
+_G.s_tab_complete = function()
+  if vim.fn.pumvisible() == 1 then
+    return t "<C-p>"
+  elseif vim.fn.call("vsnip#jumpable", {-1}) == 1 then
+    return t "<Plug>(vsnip-jump-prev)"
+  else
+    return t "<S-Tab>"
   end
 end
 
-setup_servers()
+vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
+vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
+vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
 
--- Automatically reload after `:LspInstall <server>` so we don't have to restart neovim
-require'lspinstall'.post_install_hook = function ()
-  setup_servers() -- reload installed servers
-  vim.cmd("bufdo e") -- this triggers the FileType autocmd that starts the server
-end
-
---[[ require'lspconfig'.elixirls.setup{
+require('lspconfig').elixirls.setup{
     -- Unix
     cmd = { "/home/jesse/src/elixir-ls/release/language_server.sh" };
-} ]]
-
--- completion
-paq 'nvim-lua/completion-nvim'
--- cmd([[autocmd BufEnter * lua require'completion'.on_attach() ]])
-
--- LSP
-paq 'neovim/nvim-lspconfig'
---[[ require'lspconfig'.elixirls.setup({
-    -- Unix
-    cmd = { "/home/jesse/src/elixir-ls/release/language_server.sh" },
-    --on_attach=require('completion').on_attach;
-}) ]]
-
--- Use <Tab> and <S-Tab> to navigate through popup menu
-map('i', '<Tab>', 'v:lua.smart_tab()', {expr = true, noremap = true})
-map('i', '<S-Tab>', 'v:lua.smart_shift_tab()', {expr = true, noremap = true})
+}
