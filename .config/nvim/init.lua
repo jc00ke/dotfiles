dca2cd9de2f3df64264290a944c824f6252c1208
Jesse Cooke <jesse@relativepath.io>
2021-05-17 10:55:20 -0700

    nvim: switch diagnostics bindings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -313,8 +313,8 @@ map("n", "gr",    [[<cmd>lua vim.lsp.buf.references()<CR>]], { silent = true })
 map("n", "gi",    [[<cmd>lua vim.lsp.buf.implementation()<CR>]], { silent = true })
 map("n", "K",     [[<cmd>lua vim.lsp.buf.hover()<CR>]], { silent = true })
 map("n", "<C-k>", [[<cmd>lua vim.lsp.buf.signature_help()<CR>]], { silent = true })
-map("n", "<C-n>", [[<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>]], { silent = true })
-map("n", "<C-p>", [[<cmd>lua vim.lsp.diagnostic.goto_next()<CR>]], { silent = true })
+map("n", "<Leader>N", [[<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>]], { silent = true })
+map("n", "<Leader>P", [[<cmd>lua vim.lsp.diagnostic.goto_next()<CR>]], { silent = true })
 
 
 cmd([[autocmd BufWritePre *.js lua vim.lsp.buf.formatting_sync(nil, 100)]])
