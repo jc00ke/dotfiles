d588d1dc926bacf4b2dc4a9cdcbc300a4164892b
Jesse Cooke <jesse@relativepath.io>
2021-05-24 08:58:42 -0700

    zoxide: bump to 0.7.0

--- .config/yadm/_zoxide.sh
+++ .config/yadm/_zoxide.sh
@@ -7,7 +7,7 @@ source "$DIR/_helpers.sh"
 log "Installing zoxide"
 installed_version="$(zoxide -V | cut -d " " -f 2)"
 latest_version="$(latest_release_tag_name "ajeetdsouza" "zoxide")"
-zoxide_version="0.6.0"
+zoxide_version="0.7.0"
 
 if [ "$latest_version" != "$installed_version" ]; then
   echo "Installed: $installed_version"
@@ -15,9 +15,11 @@ if [ "$latest_version" != "$installed_version" ]; then
 
   cd "$HOME/src" || exit 1
   zoxide="zoxide-x86_64-unknown-linux-musl"
-  wget "https://github.com/ajeetdsouza/zoxide/releases/download/v$zoxide_version/$zoxide"
-  sudo mv "$zoxide" "/usr/local/bin/zoxide"
+  wget "https://github.com/ajeetdsouza/zoxide/releases/download/v$zoxide_version/$zoxide.tar.gz"
+  tar xf "$zoxide.tar.gz"
+  sudo mv "$zoxide/zoxide" "/usr/local/bin/zoxide"
   sudo chmod +x "/usr/local/bin/zoxide"
+  sudo cp "$zoxide/man/* /usr/local/share/man/man1/"
 else
   echo "Already on latest: $zoxide_version"
 fi
