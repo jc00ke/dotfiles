aa2a64ea20b88148a822624383a945a8396d6fd2
Jesse Cooke <jesse@relativepath.io>
2021-07-26 12:24:11 -0700

    nvim: install vsnip
    Still need to set the mappings so LSP integration will work

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -182,8 +182,34 @@ require('packer').startup(function()
   -- Add git related info in the signs columns and popups
   use {'lewis6991/gitsigns.nvim', requires = {'nvim-lua/plenary.nvim'} }
   use 'neovim/nvim-lspconfig'        -- Collection of configurations for built-in LSP client
-  use 'hrsh7th/vim-vsnip'                      -- LSP
-  use 'hrsh7th/vim-vsnip-integ'                -- LSP
+  use {
+    'hrsh7th/vim-vsnip',                      -- LSP
+    config = function ()
+      --[[
+      " Expand
+      imap <expr> <C-j>   vsnip#expandable()  ? '<Plug>(vsnip-expand)'         : '<C-j>'
+      smap <expr> <C-j>   vsnip#expandable()  ? '<Plug>(vsnip-expand)'         : '<C-j>'
+
+      " Expand or jump
+      imap <expr> <C-l>   vsnip#available(1)  ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>'
+      smap <expr> <C-l>   vsnip#available(1)  ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>'
+
+      " Jump forward or backward
+      imap <expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>'
+      smap <expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>'
+      imap <expr> <S-Tab> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'
+      smap <expr> <S-Tab> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'
+
+      " Select or cut text to use as $TM_SELECTED_TEXT in the next snippet.
+      " See https://github.com/hrsh7th/vim-vsnip/pull/50
+      nmap        s   <Plug>(vsnip-select-text)
+      xmap        s   <Plug>(vsnip-select-text)
+      nmap        S   <Plug>(vsnip-cut-text)
+      xmap        S   <Plug>(vsnip-cut-text)onfig  function ()
+    --]]
+    end,
+    requires = {'hrsh7th/vim-vsnip-integ'}
+  }
   use 'hrsh7th/nvim-compe'           -- Autocompletion plugin
   use 'tpope/vim-sensible'                     -- defaults
   use 'tpope/vim-obsession'                    -- sessions
