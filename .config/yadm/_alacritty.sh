c9a499bd14f2ca2ffa578e5ce6e8054c01a18b97
Jesse Cooke <jesse@relativepath.io>
2021-08-04 16:45:32 -0700

    bootstrap: Bump alacritty to 0.9.0

--- .config/yadm/_alacritty.sh
+++ .config/yadm/_alacritty.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-DIR="${BASH_SOURCE%/*}" || if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
+DIR="${BASH_SOURCE%/*}" || if [[ ! -d $DIR ]]; then DIR="$PWD"; fi
 # shellcheck source=./_helpers.sh
 source "$DIR/_helpers.sh"
 
 log "Installing alacritty"
 installed_version="$(alacritty --version | cut -d " " -f 2)"
 latest_version="$(latest_release_tag_name "alacritty" "alacritty")"
-alacritty_version="0.8.0"
+alacritty_version="0.9.0"
 
 if [ "$latest_version" != "v$installed_version" ]; then
   cd "$HOME/src" || exit 1
@@ -17,19 +17,17 @@ if [ "$latest_version" != "v$installed_version" ]; then
   cargo build --release
   sudo cp target/release/alacritty /usr/local/bin
   sudo mkdir -p /usr/local/share/man/man1
-  gzip -c extra/alacritty.man | sudo tee /usr/local/share/man/man1/alacritty.1.gz > /dev/null
+  gzip -c extra/alacritty.man | sudo tee /usr/local/share/man/man1/alacritty.1.gz >/dev/null
   sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
   sudo desktop-file-install extra/linux/Alacritty.desktop
   sudo update-desktop-database
   # shellcheck disable=SC2154
   mkdir -p "${fish_complete_path[1]}"
-  cp extra/completions/alacritty.fish "${fish_complete_path[1]}/alacritty.fish"
+  cp -f extra/completions/alacritty.fish "${fish_complete_path[1]}/alacritty.fish"
   sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/local/bin/alacritty 50
   sudo update-alternatives --config x-terminal-emulator
   # to remove
   # sudo update-alternatives --remove "x-terminal-emulator" "/usr/local/bin/alacritty"
-
-  pip3 install alacritty-colorscheme
 else
   echo "Already on latest: $alacritty_version"
 fi
