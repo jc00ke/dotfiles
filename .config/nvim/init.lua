727f74ef49d61863af6dcea5db77e3619b7528ff
Jesse Cooke <jesse@relativepath.io>
2021-08-21 09:33:25 -0700

    nvim: format

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -822,9 +822,13 @@ autocmd BufWritePost *.bash Xbit
 autocmd BufWritePost *.sh Xbit ]]
 
 vim.cmd([[au FileType elixir nnoremap io o\|> IO.inspect(printable_limit: :infinity)<Esc>]])
-vim.cmd([[au FileType elixir nnoremap IO o\|> IO.inspect(label: "<C-r>=line(".")<C-M>: ", printable_limit: :infinity)<Esc>F"i]])
+vim.cmd(
+  [[au FileType elixir nnoremap IO o\|> IO.inspect(label: "<C-r>=line(".")<C-M>: ", printable_limit: :infinity)<Esc>F"i]]
+)
 vim.cmd([[au FileType elixir nnoremap ii a \|> IO.inspect(printable_limit: :infinity)<Esc>i]])
-vim.cmd([[au FileType elixir nnoremap II a \|> IO.inspect(label: "<C-r>=line(".")<C-M>: ", printable_limit: :infinity)<Esc>F"i]])
+vim.cmd(
+  [[au FileType elixir nnoremap II a \|> IO.inspect(label: "<C-r>=line(".")<C-M>: ", printable_limit: :infinity)<Esc>F"i]]
+)
 vim.cmd([[au FileType elixir nnoremap <leader>r orequire IEx; IEx.pry<esc>]])
 
 -- au FileType ruby map <Leader>fsl gg O# frozen_string_literal: true<CR><Esc>x
