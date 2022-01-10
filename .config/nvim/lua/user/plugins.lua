d1fa9a411b6531ad68e53bdfccdfa55f7bd1fe84
Jesse Cooke <jesse@relativepath.io>
2022-01-10 14:38:07 -0800

    nvim: add qf_helper

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -125,6 +125,11 @@ packer.startup(function(use)
     end,
   })
   use("nanotee/sqls.nvim")
+  use({"stevearc/qf_helper.nvim",
+  config = function ()
+    require("qf_helper").setup()
+  end
+  })
 
   if PACKER_BOOTSTRAP then
     require("packer").sync()
