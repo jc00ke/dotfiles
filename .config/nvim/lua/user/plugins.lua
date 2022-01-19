59bbb36e2005211e139a2a2e84340b72cbd54c9b
Jesse Cooke <jesse@relativepath.io>
2022-01-19 09:12:30 -0800

    nvim: don't show packer in a popup

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -36,15 +36,6 @@ if not status_ok then
   return
 end
 
--- Have packer use a popup window
-packer.init({
-  display = {
-    open_fn = function()
-      return require("packer.util").float({ border = "rounded" })
-    end,
-  },
-})
-
 packer.startup(function(use)
   use("wbthomason/packer.nvim") -- Package manager
   use("tpope/vim-fugitive") -- Git commands in nvim
