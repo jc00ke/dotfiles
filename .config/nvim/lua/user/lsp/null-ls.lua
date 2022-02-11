a29d0481d343a02f567edd6c9fe553e47f3e6532
Jesse Cooke <jesse@relativepath.io>
2022-02-10 17:00:20 -0800

    nvim+lsp: add cfn-lint to null-ls

--- .config/nvim/lua/user/lsp/null-ls.lua
+++ .config/nvim/lua/user/lsp/null-ls.lua
@@ -5,8 +5,51 @@ end
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md
 
-local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
+local formatting = null_ls.builtins.formatting
+local null_helpers = require("null-ls.helpers")
+
+local cfn_lint = {
+  method = null_ls.methods.DIAGNOSTICS,
+  filetypes = { "yaml" },
+  generator = null_helpers.generator_factory({
+    command = "cfn-lint",
+    to_stdin = true,
+    to_stderr = true,
+    args = { "--format", "parseable", "-" },
+    format = "line",
+    check_exit_code = function(code)
+      return code == 0 or code == 255
+    end,
+    on_output = function(line, params)
+      local row, col, end_row, end_col, code, message = line:match(":(%d+):(%d+):(%d+):(%d+):(.*):(.*)")
+      local severity = null_helpers.diagnostics.severities["error"]
+
+      if message == nil then
+        return nil
+      end
+
+      if vim.startswith(code, "E") then
+        severity = null_helpers.diagnostics.severities["error"]
+      elseif vim.startswith(code, "W") then
+        severity = null_helpers.diagnostics.severities["warning"]
+      else
+        severity = null_helpers.diagnostics.severities["information"]
+      end
+
+      return {
+        message = message,
+        code = code,
+        row = row,
+        col = col,
+        end_col = end_col,
+        end_row = end_row,
+        severity = severity,
+        source = "cfn-lint",
+      }
+    end,
+  }),
+}
 
 null_ls.setup({
   debug = false,
@@ -16,6 +59,7 @@ null_ls.setup({
     end
   end,
   sources = {
+    cfn_lint,
     diagnostics.credo,
     diagnostics.hadolint,
     diagnostics.shellcheck,
