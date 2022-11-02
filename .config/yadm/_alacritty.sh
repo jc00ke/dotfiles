073842d6fa9cbb7b99e97a28adf27c2906425c2d
Jesse Cooke <jesse@relativepath.io>
2022-11-02 14:55:01 -0700

    bootstrap: bump alacritty to 0.11.0

--- .config/yadm/_alacritty.sh
+++ .config/yadm/_alacritty.sh
@@ -7,7 +7,7 @@ source "$DIR/_helpers.sh"
 log "Installing alacritty"
 installed_version="$(alacritty --version | cut -d " " -f 2)"
 latest_version="$(latest_release_tag_name "alacritty" "alacritty")"
-alacritty_version="0.10.1"
+alacritty_version="0.11.0"
 
 if [ "$latest_version" != "v$installed_version" ]; then
   cd "$HOME/src" || exit 1
