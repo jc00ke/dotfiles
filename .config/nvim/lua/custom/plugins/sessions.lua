517c729f1a5a60576a27d329481e5ec0a12ee446
Jesse Cooke <jesse@relativepath.io>
2023-07-19 08:56:21 -0700

    nvim: swap autosession for persistence

--- .config/nvim/lua/custom/plugins/sessions.lua
+++ .config/nvim/lua/custom/plugins/sessions.lua
@@ -1,12 +1,11 @@
 return {
-	"rmagatti/auto-session",
-	config = function()
-		vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal"
-
-		require("auto-session").setup {
-			log_level = 'info',
-			auto_session_suppress_dirs = { '~/', '~/Projects' },
-			auto_session_enable_last_session = true
-		}
-	end
+	"folke/persistence.nvim",
+	event = "BufReadPre",
+	opts = { options = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp" } },
+	-- stylua: ignore
+	keys = {
+		{ "<leader>qs", function() require("persistence").load() end,                desc = "Restore Session" },
+		{ "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
+		{ "<leader>qd", function() require("persistence").stop() end,                desc = "Don't Save Current Session" },
+	},
 }
