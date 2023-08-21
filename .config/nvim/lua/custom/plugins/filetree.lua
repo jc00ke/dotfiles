d49c3cf8a5ce7443b6cf3a1091780d7a59b18f76
Jesse Cooke <jesse@relativepath.io>
2023-08-21 09:49:41 -0700

    nvim: switch from neo-tree to nvim-tree

--- .config/nvim/lua/custom/plugins/filetree.lua
+++ .config/nvim/lua/custom/plugins/filetree.lua
@@ -1,26 +1,19 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
+  "nvim-tree/nvim-tree.lua",
   version = "*",
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
   },
   config = function()
-    require('neo-tree').setup {}
+    require('nvim-tree').setup {}
   end,
   keys = {
     {
       "<leader>n",
-      "<cmd>NeoTreeShowToggle<cr>",
+      "<cmd>NvimTreeToggle<cr>",
       noremap = true,
       silent = true,
     },
-    {
-      "<C-n>",
-      "<cmd>NeoTreeRevealToggle<cr>",
-      noremap = true,
-      silent = true,
-    }
   }
 }
