3497055637b66961364f9ab99593a9ed80627f57
Jesse Cooke <jesse@relativepath.io>
2023-08-29 11:24:40 -0700

    swappy: add install and config file

---
+++ .config/yadm/_swappy.sh
@@ -0,0 +1,25 @@
+#!/bin/env bash
+# https://github.com/jtheoof/swappy
+
+sudo apt install \
+	scdoc \
+	gettext \
+	meson \
+	ninja-build \
+	wl-clipboard \
+	grim \
+	slurp \
+	pkg-config \
+	libcairo2-dev \
+	libgtk-3-dev \
+	libpango1.0-dev \
+	libgtk-3-0 \
+	libglib2.0-0 \
+	git
+	# otf-font-awesome
+
+cd $HOME/src
+git clone git@github.com:jtheoof/swappy.git
+meson setup build
+ninja -C build
+cp -f build/swappy $HOME/.local/bin/
