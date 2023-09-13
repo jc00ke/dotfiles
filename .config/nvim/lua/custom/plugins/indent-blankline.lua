fc565912b4ddfca7846e7b59ff4d96dd23fef70a
Jesse Cooke <jesse@relativepath.io>
2023-09-12 21:17:14 -0700

    nvim: don't show indent's current context at start

--- .config/nvim/lua/custom/plugins/indent-blankline.lua
+++ .config/nvim/lua/custom/plugins/indent-blankline.lua
@@ -11,6 +11,6 @@ return {
 		-- show_trailing_blankline_indent = false,
 		space_char_blankline = " ",
 		show_current_context = true,
-		show_current_context_start = true,
+		-- show_current_context_start = true,
 	},
 }
