05837c6e612aefcb314044fcdc75f4b8b7f7c15a
Jesse Cooke <jesse@relativepath.io>
2023-10-04 15:08:05 -0700

    nvim: disable indent-blankline for now

--- .config/nvim/lua/custom/plugins/indent-blankline.lua
+++ .config/nvim/lua/custom/plugins/indent-blankline.lua
@@ -1,17 +1,18 @@
-vim.opt.list = true
+-- vim.opt.list = true
 -- vim.opt.listchars:append "space:⋅"
 -- vim.opt.listchars:append "eol:↴"
 
 return {
 	-- Add indentation guides even on blank lines
 	'lukas-reineke/indent-blankline.nvim',
+	main = "ibl",
 	-- See `:help indent_blankline.txt`
 	opts = {
 		enabled = false,
 		-- char = '┊',
 		-- show_trailing_blankline_indent = false,
-		space_char_blankline = " ",
-		show_current_context = true,
+		-- space_char_blankline = " ",
+		-- show_current_context = true,
 		-- show_current_context_start = true,
 	},
 }
