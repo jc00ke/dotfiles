b2055824f135a864171a805eec46734334a19701
Jesse Cooke <jesse@relativepath.io>
2023-11-30 16:02:07 -0800

    Remove unused dotfiles

--- .screenlayout/jimmie.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-xrandr \
-  --output DisplayPort-0 --primary --mode 3840x2160 --pos 0x840 --rotate normal \
-  --output DisplayPort-1 --mode 3840x2160 --pos 3840x0 --rotate left \
-  --output DisplayPort-2 --off \
-  --output DisplayPort-3 --off
