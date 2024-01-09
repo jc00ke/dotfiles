54f0846dbc86f89569f92d1a7d2fdb6914c4717c
Jesse Cooke <jesse@relativepath.io>
2024-01-09 11:05:20 -0800

    nvim: use conform.nvim as formatter

---
+++ .config/nvim/lua/custom/plugins/format.lua
@@ -0,0 +1,38 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	keys = {
+		{
+			-- Customize or remove this keymap to your liking
+			"<leader>f",
+			function()
+				require("conform").format({ async = true, lsp_fallback = true })
+			end,
+			mode = "",
+			desc = "Format buffer",
+		},
+	},
+	-- Everything in opts will be passed to setup()
+	opts = {
+		-- Define your formatters
+		formatters_by_ft = {
+			javascript = { { "deno_fmt", "prettierd", "prettier" } },
+			lua = { "stylua" },
+			python = { "isort", "black" },
+			typescript = { { "deno_fmt" } },
+		},
+		-- Set up format-on-save
+		format_on_save = { timeout_ms = 250, lsp_fallback = true },
+		-- Customize formatters
+		formatters = {
+			shfmt = {
+				prepend_args = { "-i", "2" },
+			},
+		},
+	},
+	init = function()
+		-- If you want the formatexpr, here is the place to set it
+		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+	end,
+}
