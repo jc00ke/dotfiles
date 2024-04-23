ffe75835ff4e16303886ef01aacce75502fe75ff
Jesse Cooke <jesse@relativepath.io>
2024-04-23 11:41:17 -0700

    fish: add -O to CFLAGS for Erlang build

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -94,7 +94,7 @@ if test -x /opt/homebrew/bin/brew
   eval (/opt/homebrew/bin/brew shellenv)
   set -gx RUBY_YJIT_ENABLE 1
   set -gx RUBY_CONFIGURE_OPTS "--with-zlib-dir=$(brew --prefix zlib) --with-openssl-dir=$(brew --prefix openssl) --with-readline-dir=$(brew --prefix readline) --with-libyaml-dir=$(brew --prefix libyaml) --with-gdbm-dir=$(brew --prefix gdbm)"
-  set -gx CFLAGS "-Wno-error=implicit-function-declaration"
+  set -gx CFLAGS "-O2 -g -Wno-error=implicit-function-declaration"
   set -gx LDFLAGS "-L$(brew --prefix libyaml)/lib -L$(brew --prefix libffi)/lib"
   set -gx CPPFLAGS "-I$(brew --prefix libffi)/include"
 end
