6fb3f38cad201c3a35f4cd11abd34610df2c2f2a
Jesse Cooke <jesse@relativepath.io>
2024-05-16 13:25:50 -0700

    nvim: add hurl filetype

--- .config/nvim/filetype.lua
+++ .config/nvim/filetype.lua
@@ -1,4 +1,4 @@
 vim.filetype.add({
-  extension = { tfvars = "terraform", tf = "terraform", tfstate = "json", hujson = "jsonc" },
+  extension = { tfvars = "terraform", tf = "terraform", tfstate = "json", hujson = "jsonc", hurl = "hurl" },
   filename = { ["Justfile"] = "just" },
 })
