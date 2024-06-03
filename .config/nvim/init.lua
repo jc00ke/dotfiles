21ae0f75c5694161890a70e8c50a822f9f32144e
Jesse Cooke <jesse@relativepath.io>
2024-06-03 15:38:02 -0700

    nvim: telescope search through hidden dirs
    Still ignore .git dir

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -240,11 +240,22 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 })
 
 require("telescope").load_extension("zf-native")
+local telescopeConfig = require("telescope.config")
+
+-- Clone the default Telescope configuration
+local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+
+-- I want to search in hidden/dot files.
+table.insert(vimgrep_arguments, "--hidden")
+-- I don't want to search in the `.git` directory.
+table.insert(vimgrep_arguments, "--glob")
+table.insert(vimgrep_arguments, "!**/.git/*")
 
 -- [[ Configure Telescope ]]
 -- See `:help telescope` and `:help telescope.setup()`
 require("telescope").setup({
   defaults = {
+    vimgrep_arguments = vimgrep_arguments,
     mappings = {
       i = {
         ["<C-u>"] = false,
@@ -254,6 +265,12 @@ require("telescope").setup({
     sorting_strategy = "ascending",
     layout_config = {
       prompt_position = "top",
+    },
+  },
+  pickers = {
+    find_files = {
+      -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+      find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
     },
   },
 })
