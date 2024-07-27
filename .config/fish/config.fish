938259cab67b407535b8995cc9755d7547bd35a4
Jesse Cooke <jesse@relativepath.io>
2024-07-26 17:02:27 -0700

    fish+atuin: install & set up atuin

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -103,6 +103,13 @@ if test -f "$HOME/.config/op/plugins.sh"
   source "$HOME/.config/op/plugins.sh"
 end
 
+set -gx ATUIN_NOBIND "true"
+atuin init fish | source
+
+# bind to ctrl-r in normal and insert mode, add any other bindings you want here too
+bind \cr _atuin_search
+bind -M insert \cr _atuin_search
+
 mise activate fish | source
 mise hook-env | source
 mise exec -- direnv hook fish | source
