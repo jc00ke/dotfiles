e91beeb7c85173123b87e904ff42afe0b1f97c5b
Jesse Cooke <jesse@relativepath.io>
2024-10-29 11:39:52 -0700

    nvim: format

--- .config/nvim/lua/custom/plugins/indent-blankline.lua
+++ .config/nvim/lua/custom/plugins/indent-blankline.lua
@@ -4,7 +4,7 @@
 
 return {
 	-- Add indentation guides even on blank lines
-	'lukas-reineke/indent-blankline.nvim',
+	"lukas-reineke/indent-blankline.nvim",
 	main = "ibl",
 	-- See `:help indent_blankline.txt`
 	opts = {
--- .config/nvim/lua/custom/plugins/splitjoin.lua
+++ .config/nvim/lua/custom/plugins/splitjoin.lua
@@ -1,6 +1,6 @@
 return {
 	"Wansmer/treesj",
-	dependencies = { 'nvim-treesitter/nvim-treesitter', 'AndrewRadev/splitjoin.vim' },
+	dependencies = { "nvim-treesitter/nvim-treesitter", "AndrewRadev/splitjoin.vim" },
 	keys = {
 		-- { "J", "<cmd>TSJToggle<cr>", desc = "Join Toggle" },
 	},
@@ -9,21 +9,21 @@ return {
 		max_join_length = 150,
 	},
 	init = function()
-		local langs = require('treesj.langs')['presets']
+		local langs = require("treesj.langs")["presets"]
 
-		vim.api.nvim_create_autocmd({ 'FileType' }, {
-			pattern = '*',
+		vim.api.nvim_create_autocmd({ "FileType" }, {
+			pattern = "*",
 			callback = function()
 				local opts = { buffer = true }
 				if langs[vim.bo.filetype] then
-					vim.keymap.set('n', 'J', '<Cmd>TSJToggle<CR>', opts)
-					vim.keymap.set('n', 'JS', '<Cmd>TSJSplit<CR>', opts)
-					vim.keymap.set('n', 'JJ', '<Cmd>TSJJoin<CR>', opts)
+					vim.keymap.set("n", "J", "<Cmd>TSJToggle<CR>", opts)
+					vim.keymap.set("n", "JS", "<Cmd>TSJSplit<CR>", opts)
+					vim.keymap.set("n", "JJ", "<Cmd>TSJJoin<CR>", opts)
 				else
-					vim.keymap.set('n', 'JS', '<Cmd>SplitjoinSplit<CR>', opts)
-					vim.keymap.set('n', 'JJ', '<Cmd>SplitjoinJoin<CR>', opts)
+					vim.keymap.set("n", "JS", "<Cmd>SplitjoinSplit<CR>", opts)
+					vim.keymap.set("n", "JJ", "<Cmd>SplitjoinJoin<CR>", opts)
 				end
 			end,
 		})
-	end
+	end,
 }
