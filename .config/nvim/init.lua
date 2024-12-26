55189dd20a24cc628d7076640244b09997470fbf
Jesse Cooke <jesse@relativepath.io>
2024-12-26 11:12:44 -0800

    nvim+bootstrap: install clangd, clang-format and clang-tidy

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -432,7 +432,7 @@ end
 --  Add any additional override configuration in the following tables. They will be passed to
 --  the `settings` field of the server config. You must look up that documentation yourself.
 local servers = {
-  -- clangd = {},
+  clangd = {},
   -- gopls = {},
   -- pyright = {},
   -- rust_analyzer = {},
