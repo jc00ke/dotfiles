4563a1386d9afbde0b46456ed5209a13c38239cb
Jesse Cooke <jesse@relativepath.io>
2025-01-22 08:26:08 -0800

    nvim: load formatting at startup & define :Format function

--- .config/nvim/lua/custom/plugins/format.lua
+++ .config/nvim/lua/custom/plugins/format.lua
@@ -1,5 +1,6 @@
 return {
 	"stevearc/conform.nvim",
+	lazy = false,
 	event = { "BufWritePre" },
 	cmd = { "ConformInfo" },
 	keys = {
@@ -13,6 +14,19 @@ return {
 			desc = "Format buffer",
 		},
 	},
+	config = function()
+		vim.api.nvim_create_user_command("Format", function(args)
+			local range = nil
+			if args.count ~= -1 then
+				local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+				range = {
+					start = { args.line1, 0 },
+					["end"] = { args.line2, end_line:len() },
+				}
+			end
+			require("conform").format({ async = true, lsp_format = "fallback", range = range })
+		end, { range = true })
+	end,
 	-- Everything in opts will be passed to setup()
 	opts = {
 		log_level = vim.log.levels.DEBUG,
