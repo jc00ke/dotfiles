771debdca30e9b2a141963129f39e14c2e90a8c5
Jesse Cooke <jesse@relativepath.io>
2025-03-03 14:00:04 -0800

    nvim: correctly set up make ftplugin

--- .config/nvim/after/ftplugin/make.lua
+++ .config/nvim/after/ftplugin/make.lua
@@ -1,22 +1,17 @@
--- Set tab settings specifically for Makefiles
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = "make",
-	callback = function()
-		-- Use actual tabs
-		vim.bo.expandtab = false
-		-- Set tab width to 8 spaces (GNU Make standard)
-		vim.bo.tabstop = 8
-		vim.bo.shiftwidth = 8
-		-- Show tabs visually so you can see them
-		vim.wo.list = true
-		vim.opt.listchars = {
-			tab = "→ ",
-			trail = "·",
-			extends = "»",
-			precedes = "«",
-		}
-	end,
-})
+-- Use actual tabs
+vim.bo.expandtab = false
+-- Set tab width to 8 spaces (GNU Make standard)
+vim.bo.tabstop = 8
+vim.bo.shiftwidth = 8
+-- Show tabs visually so you can see them
+vim.wo.list = true
+vim.opt.listchars = {
+	tab = "→ ",
+	trail = "·",
+	extends = "»",
+	precedes = "«",
+}
+
 -- Optional: Add a command to convert existing spaces to tabs
 vim.api.nvim_create_user_command("MakefileFixIndent", function()
 	-- Preserve cursor position
