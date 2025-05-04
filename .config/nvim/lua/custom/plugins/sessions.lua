15c3a70c7b8f1f849ad233f013dd59f785a11b9b
Jesse Cooke <jesse@relativepath.io>
2025-05-04 16:03:29 -0700

    fish+nvim: switch to using neovim as MANPAGER
    disable when in +Man mode, which is actually `--embed +Man!`
    in `vim.v.argv`.

--- .config/nvim/lua/custom/plugins/sessions.lua
+++ .config/nvim/lua/custom/plugins/sessions.lua
@@ -5,6 +5,15 @@ return {
 			autoload = true,
 		})
 	end,
+	enabled = function()
+		-- disable when in man pager mode
+		for _, arg in ipairs(vim.v.argv) do
+			if arg == "+Man!" then
+				return false
+			end
+		end
+		return true
+	end,
 	init = function()
 		require("telescope").load_extension("persisted")
 	end,
