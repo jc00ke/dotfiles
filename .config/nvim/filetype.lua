722baf06aa7efa5e5c50073657ae916ba93c57f4
Jesse Cooke <jesse@relativepath.io>
2025-08-03 21:44:43 -0700

    nvim: detect dotenv filetypes

--- .config/nvim/filetype.lua
+++ .config/nvim/filetype.lua
@@ -1,4 +1,19 @@
 vim.filetype.add({
-  extension = { tfvars = "terraform-vars", tf = "terraform", tfstate = "json", hujson = "jsonc", hurl = "hurl" },
-  filename = { ["Justfile"] = "just" },
+  extension = {
+    env = "dotenv",
+    tfvars = "terraform-vars",
+    tf = "terraform",
+    tfstate = "json",
+    hujson = "jsonc",
+    hurl = "hurl"
+  },
+  filename = {
+    [".env"] = "dotenv",
+    ["Justfile"] = "just"
+  },
+  -- Detect and apply filetypes based on certain patterns of the filenames
+  pattern = {
+    -- INFO: Match filenames like - ".env.example", ".env.local" and so on
+    ["%.env%.[%w_.-]+"] = "dotenv",
+  },
 })
