db937e2427c37a5e4696e33f3a694b69ced23f33
Jesse Cooke <jesse@relativepath.io>
2025-08-04 20:31:03 -0700

    nvim: ng - add highlight on yank

--- .config/nvim-ng/init.lua
+++ .config/nvim-ng/init.lua
@@ -41,5 +41,15 @@ require("lazy").setup("plugins")
 -- completion
 -- which-key
 -- telescope
+
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = "*",
+})
 -- -- fzf
--- highlight on yank
