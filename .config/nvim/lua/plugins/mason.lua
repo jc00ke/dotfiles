ae9832d07eb04f6c714d99894a4db2b86ff5202b
Jesse Cooke <jesse@relativepath.io>
2025-08-11 14:52:34 -0700

    nvim: use mason-lspconfig & always install lua_ls & nextls

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,5 +1,13 @@
 vim.pack.add({
   { src = "https://github.com/mason-org/mason.nvim" },
+  { src = "https://github.com/mason-org/mason-lspconfig.nvim" },
+  { src = "https://github.com/neovim/nvim-lspconfig" },
 })
 
 require("mason").setup()
+require("mason-lspconfig").setup({
+  ensure_installed = {
+    "lua_ls",
+    "nextls"
+  }
+})
