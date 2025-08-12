6d516df974f26634df12b38f904cee2536de9b54
Jesse Cooke <jesse@relativepath.io>
2025-08-12 08:55:46 -0700

    nvim: extract filetypes.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -84,23 +84,3 @@ map("t", "<esc>", [[<c-\><c-n>]])
 map("t", "<c-o>", [[<c-\><c-n>]])
 map('t', '', "")
 map('t', '', "")
-
-vim.filetype.add({
-  extension = {
-    env = "dotenv",
-    tfvars = "terraform-vars",
-    tf = "terraform",
-    tfstate = "json",
-    hujson = "jsonc",
-    hurl = "hurl"
-  },
-  filename = {
-    [".env"] = "dotenv",
-    ["Justfile"] = "just"
-  },
-  -- Detect and apply filetypes based on certain patterns of the filenames
-  pattern = {
-    -- INFO: Match filenames like - ".env.example", ".env.local" and so on
-    ["%.env%.[%w_.-]+"] = "dotenv",
-  },
-})
