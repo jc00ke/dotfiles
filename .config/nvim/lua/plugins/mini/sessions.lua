5071c45e7291816af22a5f9d00b53cc6561a813a
Jesse Cooke <jesse@relativepath.io>
2025-08-12 12:25:54 -0700

    nvim: customize session name based on current path

--- .config/nvim/lua/plugins/mini/sessions.lua
+++ .config/nvim/lua/plugins/mini/sessions.lua
@@ -2,7 +2,17 @@ vim.pack.add({
   { src = "https://github.com/echasnovski/mini.sessions" },
 })
 
+local current_dir = vim.fn.getcwd():gsub("%.", "__dot__"):gsub("/", "__"):gsub("\\\\", "__")
+
 require('mini.sessions').setup({
   autoread = true,
-  directory = "mini-sessions"
+  autowrite = false,
+  file = "",
+  directory = vim.fn.stdpath('state') .. "/sessions"
 })
+
+
+local make_session = function()
+  require("mini.sessions").write(current_dir, { force = true })
+end
+vim.api.nvim_create_autocmd('VimLeavePre', { callback = make_session, desc = 'Make session' })
