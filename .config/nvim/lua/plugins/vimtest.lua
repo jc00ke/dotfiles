41d425308e966edf114d4c6ef0a8770d00e2dbc6
Jesse Cooke <jesse@relativepath.io>
2025-09-11 17:10:56 -0700

    nvim: change vimtest keymaps
    I don't like these, but I think Mini.Clue is intercepting and its delay
    is getting in the way.

--- .config/nvim/lua/plugins/vimtest.lua
+++ .config/nvim/lua/plugins/vimtest.lua
@@ -2,8 +2,8 @@ vim.pack.add({
   { src = "https://github.com/vim-test/vim-test" },
 })
 
-vim.keymap.set("n", "<leader>t", "<cmd>TestNearest<cr>", { desc = "Run nearest test" })
-vim.keymap.set("n", "<leader>l", "<cmd>TestLast<cr>", { desc = "Run last test" })
+vim.keymap.set("n", "<leader>tt", "<cmd>TestNearest<cr>", { desc = "Run nearest test" })
+vim.keymap.set("n", "<leader>tl", "<cmd>TestLast<cr>", { desc = "Run last test" })
 vim.keymap.set("n", "<leader>T", "<cmd>TestFile<cr>", { desc = "Run all tests in the file" })
 vim.keymap.set("n", "<leader>ta", "<cmd>TestSuite<cr>", { desc = "Run all tests" })
 vim.keymap.set("n", "<leader>tv", "<cmd>TestVisit<cr>",
