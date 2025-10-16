367d4b96caa3afdd975a2ce0a8fd1ab469df9969
Jesse Cooke <jesse@relativepath.io>
2025-10-16 10:12:50 -0700

    fish: add homebrew to path on Mac

--- .config/fish/darwin.fish
+++ .config/fish/darwin.fish
@@ -9,6 +9,7 @@ set -x MIX_OS_DEPS_COMPILE_PARTITION_COUNT (
 
 if test -x /opt/homebrew/bin/brew
     eval (/opt/homebrew/bin/brew shellenv)
+    fish_add_path /opt/homebrew/bin/
     set -gx RUBY_YJIT_ENABLE 1
     set -gx RUBY_CONFIGURE_OPTS "--with-zlib-dir=$(brew --prefix zlib) --with-openssl-dir=$(brew --prefix openssl) --with-readline-dir=$(brew --prefix readline) --with-libyaml-dir=$(brew --prefix libyaml) --with-gdbm-dir=$(brew --prefix gdbm)"
     set -gx CFLAGS "-O2 -g -Wno-error=implicit-function-declaration"
