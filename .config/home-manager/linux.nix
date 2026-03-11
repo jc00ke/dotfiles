2506ef34af5ebd4f0f0b729c69ed4b26151bd3cb
Jesse Cooke <jesse@relativepath.io>
2026-03-11 15:14:18 -0700

    bootstrap: install ghostty via Debian package
    Remove install via home-manager.

--- .config/home-manager/linux.nix
+++ .config/home-manager/linux.nix
@@ -1,60 +1,13 @@
 { config, pkgs, lib, ... }:
 
 let
-  vgaOutput = builtins.readFile (pkgs.runCommand "gpu-check" {} ''
-   ${pkgs.pciutils}/bin/lspci > $out
-  '');
-  offloadWrapper = if builtins.match ".*VGA.*Intel.*" vgaOutput != null
-    then "intel"
-  else if builtins.match ".*VGA.*Radeon.*" vgaOutput != null
-    then "radeon"
-  else
-    null;
   
 in
 {
-  # https://github.com/nix-community/nixGL?tab=readme-ov-file#nix-channel-recommended
-  # nix-channel --add https://github.com/nix-community/nixGL/archive/main.tar.gz nixgl && nix-channel --update
-  targets.genericLinux.nixGL.packages = import <nixgl> { inherit pkgs; };
-  targets.genericLinux.nixGL.defaultWrapper = "mesa";
-  targets.genericLinux.nixGL.offloadWrapper = offloadWrapper;
 
   home.packages = [
     pkgs.swappy
   ];
-
-  # https://nix-community.github.io/home-manager/index.xhtml#sec-usage-gpu-non-nixos
-  programs.ghostty = {
-    enable = true;
-    package = config.lib.nixGL.wrap pkgs.ghostty;
-  };
-
-  # https://github.com/ghostty-org/ghostty/discussions/3763#discussioncomment-11699970
-  xdg.desktopEntries."com.mitchellh.ghostty" = {
-    name = "Ghostty";
-    type = "Application";
-    comment = "A terminal emulator";
-    exec = "ghostty";
-    icon = "com.mitchellh.ghostty";
-    terminal = false;
-    startupNotify = true;
-    categories = [ "System" "TerminalEmulator" ];
-    settings = {
-      Keywords = "terminal;tty;pty;";
-      X-GNOME-UsesNotifications = "true";
-      X-TerminalArgExec = "-e";
-      X-TerminalArgTitle = "--title=";
-      X-TerminalArgAppId = "--class=";
-      X-TerminalArgDir = "--working-directory=";
-      X-TerminalArgHold = "--wait-after-command";
-    };
-    actions = {
-      new-window = {
-        name = "New Window";
-        exec = "ghostty";
-      };
-    };
-  };
 
   # https://discourse.nixos.org/t/apps-installed-via-home-manager-are-not-visible-within-gnome/48252/2
   home.activation.copyDesktopFiles = lib.hm.dag.entryAfter ["installPackages"] ''
