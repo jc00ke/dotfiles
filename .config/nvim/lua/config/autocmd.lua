0fa14e6694f82616abd48f83d384e92c2098df8d
Jesse Cooke <jesse@relativepath.io>
2026-04-20 15:06:29 -0700

    nvim: add autorefresh toggle

--- .config/nvim/lua/config/autocmd.lua
+++ .config/nvim/lua/config/autocmd.lua
@@ -39,3 +39,34 @@ vim.api.nvim_create_autocmd('LspAttach', {
     end
   end,
 })
+
+-- autocmd
+--------------------------------------------------------------------------------
+-- Custom function to toggle auto refresh of buffer after file changes
+---@diagnostic disable-next-line: param-type-mismatch
+vim.g.auto_refresh_enabled = false
+
+-- Function to toggle the behavior
+function ToggleAutoRefresh()
+  if vim.g.auto_refresh_enabled then
+    vim.api.nvim_clear_autocmds({ group = "AutoRefresh" })
+    vim.g.auto_refresh_enabled = false
+    print("Auto refresh disabled")
+  else
+    vim.o.autoread = true
+    vim.api.nvim_create_augroup("AutoRefresh", { clear = true })
+    vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter" }, {
+      group = "AutoRefresh",
+      command = "if mode() != 'c' | checktime | endif",
+      pattern = "*",
+    })
+    vim.g.auto_refresh_enabled = true
+    print("Auto refresh enabled")
+  end
+end
+
+vim.keymap.set('n', '<leader>ar', ToggleAutoRefresh, {
+  noremap = true,
+  silent = false,
+  desc = "Toggle auto refresh of files"
+})
