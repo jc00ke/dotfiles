6db5341c410f84d3c2d0afa2f6d0943b43aa9a6d
Jesse Cooke <jesse@relativepath.io>
2026-04-30 17:15:58 -0700

    nvim: make sure to use main branch for ts

--- .config/nvim/plugin/treesitter.lua
+++ .config/nvim/plugin/treesitter.lua
@@ -1,5 +1,5 @@
 vim.pack.add({
-  { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+  { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
 })
 
 if vim.fn.has('win32') == 1 then
