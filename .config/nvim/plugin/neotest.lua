07f7392858cc46b04a1fdf4b53ccdaa03df2665b
Jesse Cooke <jesse@relativepath.io>
2026-05-08 11:03:39 -0700

    nvim: bump treesitter
    This was pretty busted on my Mac laptop, turns out
    it was from the package lock rev for ts being wrong and
    vim.pack.update wasn't updating it after the branch switch
    to main.

--- .config/nvim/plugin/neotest.lua
+++ .config/nvim/plugin/neotest.lua
@@ -3,7 +3,7 @@ vim.pack.add({
   { src = "https://github.com/nvim-neotest/nvim-nio", },
   { src = "https://github.com/nvim-lua/plenary.nvim", },
   { src = "https://github.com/antoinemadec/FixCursorHold.nvim", },
-  { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+  { src = "https://github.com/nvim-treesitter/nvim-treesitter", branch = "main" },
   { src = "https://github.com/fredrikaverpil/neotest-golang" },
 })
 
