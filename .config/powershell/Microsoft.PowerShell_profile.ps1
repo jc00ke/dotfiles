c8b3e5473138c3042f7d227e2b4b6c728bfd7ab7
Jesse Cooke <jesse@relativepath.io>
2026-05-14 16:03:03 -0700

    pwsh: port profile from laptop

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,6 @@
-Write-Host "Hi there!"
 (&mise activate pwsh) | Out-String | Invoke-Expression
+(&mise completion powershell) | Out-String | Invoke-Expression
+
+Set-Alias -Name e -Value nvim
+function Quit { Invoke-Command -ScriptBlock { exit } }
+Set-Alias -Name xx -Value Quit
