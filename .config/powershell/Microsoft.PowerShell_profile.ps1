dc2d6e8ed1e5909009ae8c85d993ecd765e4b519
Jesse Cooke <jesse@relativepath.io>
2026-05-19 12:39:43 -0700

    pwsh: install PSScriptAnalyzer for Invoke-Formatter

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,21 @@
-(&mise activate pwsh) | Out-String | Invoke-Expression
+(mise activate pwsh) | Out-String | Invoke-Expression
 (&mise completion powershell) | Out-String | Invoke-Expression
 
 Set-Alias -Name e -Value nvim
 function Quit { Invoke-Command -ScriptBlock { exit } }
 Set-Alias -Name xx -Value Quit
+
+if (-not (Get-Command Invoke-Formatter -ErrorAction SilentlyContinue)) {
+    $ProgressPreference = 'SilentlyContinue'
+    Install-Module -Name PSScriptAnalyzer -Scope CurrentUser -Force -Confirm:$false
+}
+
+# Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
+#     param($wordToComplete, $commandAst, $cursorPosition)
+#         [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+#         $Local:word = $wordToComplete.Replace('"', '""')
+#         $Local:ast = $commandAst.ToString().Replace('"', '""')
+#         winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+#             [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+#         }
+# }
