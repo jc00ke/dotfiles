3fc38b8d94c51e3466e803260211feb807335785
Jesse Cooke <jesse@relativepath.io>
2026-07-14 14:58:30 -0700

    nvim: add rumdl lsp config

---
+++ .config/nvim/lsp/rumdl.lua
@@ -0,0 +1,10 @@
+return {
+  cmd = { "rumdl", "server" },
+  filetypes = { "markdown" },
+  root_markers = { ".git", ".rumdl.toml" },
+  settings = {
+    rumdl = {
+      lineLength = 110,
+    }
+  }
+}
