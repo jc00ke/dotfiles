792dac548b1de414604e4bc6552f121b52d67633
Jesse Cooke <jesse@relativepath.io>
2026-08-26 18:23:28 -0700

    nvim: add quicker.nvim

---
+++ .config/nvim/plugin/quicker.lua
@@ -0,0 +1,32 @@
+vim.pack.add({
+  { src = "https://github.com/stevearc/quicker.nvim" },
+})
+
+vim.keymap.set("n", "<leader>q", function()
+  require("quicker").toggle()
+end, {
+  desc = "Toggle quickfix",
+})
+vim.keymap.set("n", "<leader>l", function()
+  require("quicker").toggle({ loclist = true })
+end, {
+  desc = "Toggle loclist",
+})
+require("quicker").setup({
+  keys = {
+    {
+      ">",
+      function()
+        require("quicker").expand({ before = 2, after = 2, add_to_existing = true })
+      end,
+      desc = "Expand quickfix context",
+    },
+    {
+      "<",
+      function()
+        require("quicker").collapse()
+      end,
+      desc = "Collapse quickfix context",
+    },
+  },
+})
