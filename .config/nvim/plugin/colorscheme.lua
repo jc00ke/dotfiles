0b3eff0d86085e129a8c439868ca3356cdcc098e
Jesse Cooke <jesse@relativepath.io>
2026-09-15 10:48:26 -0700

    nvim+ghostty: switch to Catppuccin Latte
    I think this has better support in Windows Terminal, so let's
    give it a try.

--- .config/nvim/plugin/colorscheme.lua
+++ .config/nvim/plugin/colorscheme.lua
@@ -1,12 +1,3 @@
 vim.pack.add({
-  -- { src = 'https://github.com/rockorager/radix.nvim' },
-  -- { src = "https://github.com/Mofiqul/adwaita.nvim" },
-  { src = "https://github.com/craftzdog/solarized-osaka.nvim" },
+  { src = "https://github.com/catppuccin/nvim", name = "catppuccin" },
 })
-
--- require("radix").setup({
---   style = "light"
--- })
--- vim.cmd.colorscheme("radix")
-
-vim.cmd.colorscheme("solarized-osaka")
